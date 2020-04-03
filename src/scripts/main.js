f15f6d0262a3044584be3a41ec577ae88c50a0fd
Stanislav Mochyr <delenjer@gmail.com>
2020-04-03 17:14:04 +0300

    create slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// eslint-disable-next-line no-unused-vars,no-undef
+const swiper = new Swiper('.swiper-container, .swiper-container2', {
+  spaceBetween: 30,
+  effect: 'fade',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
