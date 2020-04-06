a2c71179797c13010094a922869294f7ba29e933
Stanislav Mochyr <delenjer@gmail.com>
2020-04-06 17:49:00 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,32 @@ const swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+const menuShow = () => {
+  const showMenu = document.querySelector('.show-menu');
+  const hideMenu = document.querySelector('.close-menu');
+  const nav = document.querySelector('.nav');
+  const navItem = document.querySelector('.nav__list');
+
+  showMenu.addEventListener('click', () => {
+    nav.classList.add('show-nav');
+    nav.classList.remove('hide-nav');
+  });
+
+  hideMenu.addEventListener('click', () => {
+    nav.classList.add('hide-nav');
+    nav.classList.remove('show-nav');
+  });
+
+  hideMenu.addEventListener('click', () => {
+    nav.classList.add('hide-nav');
+    nav.classList.remove('show-nav');
+  });
+
+  navItem.addEventListener('click', () => {
+    nav.classList.remove('show-nav');
+    nav.classList.add('hide-nav');
+  });
+};
+
+menuShow();
